848b721a53c6316ea9186872f3cf48c72d27eae9
ilya segal <65099651+ilyaVenom@users.noreply.github.com>
2023-09-16 22:10:31 -0400

    Add files via upload

---
+++ Hey_u_py.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 16 21:54:38 2023
+@author: segal
+"""
+# start with an outline
+'''
+First a few comments:
+# ask user for name
+# ask user for their age
+# ask user for city
+# ask user what they like doing
+plus create output txt
+and
+print output to screen
+'''
+givenName = input("What's your name bro? \n")
+
+Age = input("How old are u dude? \n")
+
+city = input("What's your city? \n")
+
+hobbies = input("What do you like doing? \n")
+
+print("Hi, I am", givenName, "and I am", Age,"\nAnd my city is ", city,
+      "\nPlus I like doing ", hobbies)
+
+
